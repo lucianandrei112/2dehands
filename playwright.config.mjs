975ea0e99cb38c8d720156cc32efee9abdfd50cd
lucianandrei112 <152749851+lucianandrei112@users.noreply.github.com>
2025-09-02 14:42:26 +0200

    Create playwright.config.mjs

---
+++ playwright.config.mjs
@@ -0,0 +1,7 @@
+export default {
+  use: {
+    headless: true,
+    locale: 'nl-BE',
+    userAgent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36',
+  }
+}
